f175c01f489a3cc43a0248f940a9c5647a160866
Evania Torres <106711685+Sterling2Archer@users.noreply.github.com>
2023-03-03 14:33:04 -0500

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Who from '../components/Who';
+import Footer from '../components/Footer';
+
+const About = () => {
+  return (
+    <div>
+      <Who />
+      <Footer />
+    </div>
+  )
+};
+
+export default About;
---
+++ src/Orders.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Buying from '../components/Buying';
+import Footer from '../components/Footer';
+
+const Orders = () => {
+  return (
+    <div>
+        <Buying />
+        <Footer />
+    </div>
+  )
+};
+
+export default Orders;
