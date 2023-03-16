92be193697c063a3e4eea277f1f567924f2b89da
Evania Torres <106711685+Sterling2Archer@users.noreply.github.com>
2023-03-15 21:10:19 -0400

    Update index.js
    Updating code with GitHub pages version to keep track of differences between Browser Router vs Hash Router code.

--- src/index.js
+++ src/index.js
@@ -10,3 +10,32 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
+
+/*
+CODE FOR GITHUB PAGES (HASH ROUTER)
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Link } from 'react-router-dom';
+import './index.css'
+import App from './App';
+
+ReactDOM.render(
+    <HashRouter basename="/">     
+       <App />
+       
+      <div className='footer'>
+       <div className="container">         
+          <Link to="/" className='btn'>home </Link>
+          <Link to="/about" className='btn'>about us </Link>
+          <Link to="/orders" className='btn'>releases </Link>
+          <Link to="/contacting" className='btn'>contact us </Link>      
+         
+        <strong> &copy;Evania Torres 2023</strong>
+      </div>
+   </div>   
+</HashRouter>,
+  document.getElementById('root')
+);
+
+/*
