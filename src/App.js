c0a13d47da6ed7df6ef2920876487c48d895502a
Evania Torres <106711685+Sterling2Archer@users.noreply.github.com>
2023-03-15 21:11:21 -0400

    Update App.js
    Updating code with GitHub pages version to keep track of differences between Browser Router vs Hash Router code.

--- src/App.js
+++ src/App.js
@@ -20,3 +20,43 @@ function App() {
 };
 
 export default App;
+
+/*
+
+Code for GitHub Pages
+
+import React, { useState } from 'react'
+import './App.css'
+import Home from './routes/Home';
+import Orders from './routes/Orders';
+import About from './routes/About';
+import Contacting from "./routes/Contacting";
+import { Route, Routes } from 'react-router-dom';
+import enstock from './assets/peacechain-done25.png';
+import Navbar from './components/Navbar';
+
+function App() {
+  const [theme, setTheme] = useState('theme' ? 'dark' : 'light')
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme)
+  }
+  return (
+    <div className="app" data-theme={theme}>  <Navbar />
+        <div className='theme-toggle'> 
+        <img onClick={switchTheme} src={enstock} class="enstocknyc" alt="ESNYC" />
+       
+    <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/orders' element={<Orders />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contacting' element={<Contacting />} />
+        </Routes>
+
+        </div>
+      </div>
+  );
+}
+
+export default App;
+/*
